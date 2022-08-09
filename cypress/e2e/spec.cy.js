1dc8392cc67062a8a22581e1af3aaa6cf28132b0
Michael J. Harrison <95496577+mikeharrison57@users.noreply.github.com>
2022-08-09 10:00:54 -0600

    Started cypress testing and set up fixture

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,5 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('URL Shortener, main page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls')
   })
 })
